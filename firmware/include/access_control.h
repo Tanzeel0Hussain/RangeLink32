31f3c1e1982b1da1bc050348491fec03a2209bf3
Tanzeel Hussain <tanzeelhussain38529@gmail.com>
2026-09-19 13:24:18 +0500

    feat: define client access-control interface

---
+++ firmware/include/access_control.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <Arduino.h>
+
+enum class AccessMode : uint8_t {
+  AllowAll = 0,
+  AllowlistOnly = 1
+};
+
+void accessControlBegin();
+void accessControlLoop();
+
+AccessMode getAccessMode();
+void setAccessMode(AccessMode mode);
+
+bool setClientApproval(const String& mac, bool approved);
+bool setClientBlocked(const String& mac, bool blocked);
+bool clientMayUseInternet(const String& mac);
+String getClientTableJson();
